859281573887bf5f9fd28ac03be137feaea068d8
David Toro Triana <dtorot@opensai.org>
2024-03-01 18:55:34 -0500

    Update parameters and info to run web.sh...

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # The Poor Man Web Tools
-Scripts and tools list used in daily web maintenance, updating and content creation, in the old and dirty work style... 
+Scripts and tools list used in daily web maintenance, updating and content creation, in the old and dirty work style...
+
+### web image resizing **web.sh** 
+Bash script used to resize a group o images to be uploaded to the web:
+- must be executed in the folder that contains the images
+- must be parameterized (edit the bash script):
+    - IMG_FORMAT=jpeg > what type of image do you convert to web size?
+    - IMG_COUNTER=12 > if is not equal to zero, the new images will be incrementally named 1.jpg, 2.jpg... etc
+    - IMG_SIZE=1600x1200 > size in pixels of the new image
+    - IMG_QUALITY=70 > quality percentage of the new image vs. the original
+    - CLEAN_FILE_NAMES=1 > use to clean file name with spaces
+- the script use the tool [detox](https://detox.sourceforge.net/) to clean the file names, available in the standard repos of the main linux distros
--- web.sh
+++ web.sh
@@ -1,8 +1,30 @@
 #!/bin/bash
+
+# replace main parameters
+IMG_FORMAT=jpeg # what type of image do you convert to web size?
+IMG_COUNTER=12 # if is not equal to zero, the new images will be incrementally named 1.jpg, 2.jpg... etc
+IMG_SIZE=1600x1200 # size in pixels of the new image
+IMG_QUALITY=70 # quality percentage of the new image vs. the original
+CLEAN_FILE_NAMES=1 # use to clean file name with spaces
+
+
+clear
 echo "Batch process to resize images in jpg format to upload to web root"
-for imagen in $(ls *.jpg);do 
+
+if [[ $CLEAN_FILE_NAMES != 0 ]]; then
+	detox -v .
+fi
+
+for imagen in $(ls *.$IMG_FORMAT);do 
 	echo Processing image $imagen, Original size[$(du -h $imagen)]
-	convert $imagen -verbose -monitor -resize 50% -quality 70 $imagen
-	echo ended process in $imagen, New size[$(du -h $imagen)]
-	echo ------------------------------------------------------------
+	if [[ $IMG_COUNTER == 0 ]]; then
+		convert $imagen -verbose -monitor -resize $IMG_SIZE -quality $IMG_QUALITY $imagen
+		echo ended process in $imagen, New size[$(du -h $imagen)]
+		echo ------------------------------------------------------------
+	else
+		convert $imagen -verbose -monitor -resize $IMG_SIZE -quality $IMG_QUALITY $IMG_COUNTER.$IMG_FORMAT
+		echo ended process in $IMG_FORMAT.$IMG_COUNTER, New size[$(du -h $IMG_COUNTER.$IMG_FORMAT)]
+		echo ------------------------------------------------------------
+		let IMG_COUNTER++
+	fi
 done
